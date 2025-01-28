3970512dc4c111abe3ad64230779eceeec560556
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2025-01-28 12:27:45 +0100

    Add banner

--- assets/js/why-us.js
+++ assets/js/why-us.js
@@ -1,6 +1,6 @@
 // Why choose us
 const whyImageElement = document.getElementById("why-us-image");
-const whyImages = ["facility1.jpg", "dumbell.jpg", "equipments.jpg", "gym6.jpg", "gym8.jpg"];
+const whyImages = ["facility1.jpg", "dumbell.jpg", "equipments.jpg", "gym6.jpg", "gym8.jpg", "fac2.jpg", "fac3.jpg"];
 
 window.addEventListener("DOMContentLoaded", () => {
     let currentIndex = 0;
